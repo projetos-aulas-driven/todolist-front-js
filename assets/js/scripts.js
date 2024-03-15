1bc4c4f6f42dcc08538b108f82f4466e21f26dcd
leticia-chijo <leticia.chijo@gmail.com>
2024-03-15 14:38:19 -0300

    Aula - Manipulação do DOM (Array), Loop for

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,45 @@
 let contador = 3;
+const tarefasTutorial = [
+    "Adicione uma tarefa no botão acima ☝️",
+    "Passe o mouse na tarefa para ver o botão de excluir 🗑️",
+    "Clique na tarefa para marcá-la como feita ✔️"
+];
+
+function adicionarTarefasTutorial() {
+    const ul = document.querySelector('ul');
+
+    for (let index = 0; index < tarefasTutorial.length; index++) {
+        const elementoTarefa = `
+            <li>
+                <div class="btn-delete">
+                    <ion-icon name="trash-outline"></ion-icon>
+                </div>
+                <span onclick="finalizarTarefa(this)">${tarefasTutorial[index]}</span>
+            </li>
+        `;
+
+        ul.innerHTML += elementoTarefa;
+    }
+}
+
+adicionarTarefasTutorial();
+
+function adicionarNovaTarefa() {
+    const novaTarefa = document.querySelector("input").value;
+    let ul = document.querySelector("ul");
+    
+    ul.innerHTML += `
+        <li>
+            <div class="btn-delete">
+                <ion-icon name="trash-outline"></ion-icon>
+            </div>
+            <span onclick="finalizarTarefa(this)">${novaTarefa}</span>
+        </li>
+    `;
+    document.querySelector("input").value = "";
+    contador++;
+    atualizarContador();
+}
 
 function finalizarTarefa(elemento) {
     const li = elemento.parentNode;
